a7bfb972359cd74edee7afd4b9ccb397353259e3
ok <kelsey.collins23@d155.org>
2022-09-07 07:57:29 -0500

    yum

--- Mod 2 Creative App Calculator/ViewController.swift
+++ Mod 2 Creative App Calculator/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outletTectFieldOne: UITextField!
+    @IBOutlet weak var outletTextFieldTwo: UITextField!
+    @IBOutlet weak var outletAnswer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func actionAdd(_ sender: Any) {
+        outletAnswer.text = String(Double(outletTectFieldOne.text ?? "0")! + Double(outletTextFieldTwo.text ?? "0")!)
+    }
+    
+    @IBAction func actionSub(_ sender: Any) {
+        outletAnswer.text = String(Double(outletTectFieldOne.text ?? "0")! - Double(outletTextFieldTwo.text ?? "0")!)
+        
+    }
+    
+    
 }
 
